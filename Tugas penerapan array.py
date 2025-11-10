07c7d49390cd002397ae0bd37ed2715fa5ee902c
William Okta Jaya <williamoktajaya@gmail.com>
2025-11-10 12:53:58 +0800

    Tugas penerapan array
    Nama : William Okta Jaya 
    NIM : D0425510
    Prodi : Sistem Informasi

---
+++ Tugas penerapan array.py
@@ -0,0 +1,17 @@
+#Nama : William Okta Jaya
+#NIM : D0425510
+#Prodi : Sistem Informasi
+#Tugas Penerapan Array pada, Nama,Nim dan Prodi
+
+#data
+
+data =["William Okta Jaya","D0425510","Sistem Informasi"]
+print(data[0])
+print(data[1])
+print(data[2])
+
+# ( Penjelasan ) Array adalah struktur data yang digunakan untuk menyimpan sekumpulan nilai atau elemen dengan tipe data yang sama dalam satu variabel, dan setiap elemen dapat diakses menggunakan indeks (nomor urut).
+
+# ( Penjelasan ) Array berfungsi seperti wadah yang menampung banyak data sejenis (misalnya semua angka, semua huruf, atau semua string).
+
+# ( Penjelasan ) Elemen pertama array biasanya memiliki indeks 0 (tergantung bahasa pemrograman).
